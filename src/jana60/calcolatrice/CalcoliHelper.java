13736eb27429f946dd20b57a6f9a22c2ec71da24
Pier95F <forcinapierpaolo@gmail.com>
2022-06-27 12:33:20 +0200

    esercizio base terminato

--- src/jana60/calcolatrice/CalcoliHelper.java
+++ src/jana60/calcolatrice/CalcoliHelper.java
@@ -1,5 +1,65 @@
 package jana60.calcolatrice;
 
 public class CalcoliHelper {
+	
+/* Creare una classe di helper CalcoliHelper. Come visto a lezione, strutturare la classe in modo che non possa essere istanziata (cioè non posso creare oggetti di tipo CalcoliHelper con la parola chiave new) e che presenti i seguenti metodi static:
+	- Somma di due numeri interi
+	- Differenza tra due numeri interi
+	- Moltiplicazione di due numeri interi
+	- Valore assoluto di un numero intero
+	- Minimo tra due numeri interi
+	- Massimo tra due numeri interi */
+	
+	private CalcoliHelper() {
+		
+	}
 
+	// Definisco i metodi:
+	
+	// 1- Somma di due numeri interi
+	public static int somma (int x, int y) {
+		return x+y;
+	}
+	
+	// 2 - Differenza tra due numeri interi
+	public static int differenza (int x, int y) {
+		if (x> y) {
+			return x-y;
+		} else {
+			return y-x;
+		}
+	}
+	
+	// 3 - Moltiplicazione di due numeri interi
+	public static int moltiplicazione (int x, int y) {
+		return x*y;
+	}
+	
+	// 4 - Valore assoluto di un numero intero
+	public static int valoreAssoluto (int x) {
+		if (x>=0) {
+			return x;
+		} else {
+			return x*-1;
+		}
+	}
+	
+	// 5 - Minimo tra due numeri interi
+	public static int numeroMinimo (int x, int y) {
+		if (x<y) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+	
+	// 6 - Massimo tra due numeri interi
+	public static int numeroMassimo (int x, int y) {
+		if (x>y) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+	
 }
--- src/jana60/calcolatrice/CalcoliTest.java
+++ src/jana60/calcolatrice/CalcoliTest.java
@@ -4,7 +4,14 @@ public class CalcoliTest {
 
 	public static void main(String[] args) {
 		
-
+		// Testo i metodi della classe CalcoliHelper
+		
+		System.out.println("Somma: " + CalcoliHelper.somma(7, 25));
+		System.out.println("Differenza: " + CalcoliHelper.differenza(7, 25));
+		System.out.println("Moltiplicazione: " + CalcoliHelper.moltiplicazione(7, 25));
+		System.out.println("Valore assoluto: " + CalcoliHelper.valoreAssoluto(-7));
+		System.out.println("Numero minimo: " + CalcoliHelper.numeroMinimo(7, 25));
+		System.out.println("Numero massimo: " + CalcoliHelper.numeroMassimo(7, 25));
 	}
 
 }
